eb26f22a7fe8eab1ef2ca0b08539d56458947f57
keystroke01 <80722661+keystroke01@users.noreply.github.com>
2022-01-15 07:59:51 +0400

    uploaded

---
+++ aliases.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# CUSTOM ALIASES
+alias ll='ls -lah --color=auto'
+alias l1='ls -1 --color=auto'
+alias scrcpy='scrcpy -S'
+
+# ytfzf ALIASES
+alias yt="ytfzf -t"
+alias yts="ytfzf -t -S"
+alias yta="ytfzf -t -m"
+alias ytsa="ytfzf -t -S -m"
+
+# git/github-cli Aliases
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push -u origin"
+alias gs="git status"
